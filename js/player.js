90d6eb03fa0619447c800099aea75f14dd4f4a34
Farid Rn <faridv@gmail.com>
2024-05-15 02:41:58 +0330

    added documentation

--- js/player.js
+++ js/player.js
@@ -1,9 +1,17 @@
+/* 
+ * The initial default values for each player object are later merged with the given data.
+ * If a property is missing, the default value will be used.
+ */
 const defaultPlayerData = {
   name: 'Player',
   hand: '',
   score: 0,
 };
 
+/*
+ * This class is responsible for handling each player's state
+ * After instantiating, it is possible to set the latest move/hand of the player and its score.
+ */
 export default class Player {
 
   hand;
